5fa0e081ecd2f29e8792bfd11090387cb2d5fb40
Prihan Nimsara <Iamprihan@gmail.com>
2019-01-14 15:55:21 +0530

    complete

--- Project/src/code/HelloPython.py
+++ Project/src/code/HelloPython.py
@@ -1,2 +1,21 @@
-x,e,r=2,3,4
-print(e)
+variable1 = variable2 = variable3 = 100;
+print(variable1)
+print(variable2)
+print(variable3)
+
+variable1,variable2,variable3 = 10,20,30
+print(variable1)
+print(variable2)
+print(variable3)
+
+tuple = (1,"prihan")
+print(tuple[0])
+
+tuple2 = (2,"nimsara")
+print(tuple2[0])
+print(tuple2)
+print(tuple +  tuple2)
+
+dictionary = {1:'prihan','ab':'nimsara'}
+print(dictionary.keys())
+print(dictionary.values())
